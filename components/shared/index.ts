4820444498c5cca383012a7f2cdc7b4c95282c67
Robert Walker <robertWalker6580@gmail.com>
2023-12-23 21:37:14 -0800

    fixed error in shared index

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -2,8 +2,5 @@ import Header from "@/components/shared/Header";
 import Footer from "@/components/shared/Footer";
 import NavItems from "@/components/shared/NavItems";
 import MobileNav from "@/components/shared/MobileNav";
-// import CategoryFilter from "@/components/shared/CategoryFilter";
-// import Collection from "@/components/shared/Collection";
-// import Search from "@/components/shared/Search";
 
 export { Header, Footer, NavItems, MobileNav };
