053eb8c8db2f53d733f0e443f5317cd5a1200587
Robert Walker <robertWalker6580@gmail.com>
2023-12-23 21:17:09 -0800

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Header, Footer } from "@/components/shared";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
